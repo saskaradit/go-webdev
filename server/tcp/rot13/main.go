7f411e73df1bad052393a5079e33851dc89b20c2
Raditya Saskara <saskaradit@gmail.com>
2021-01-19 21:22:55 +0700

    multiplexer

---
+++ server/tcp/rot13/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := strings.ToLower(scanner.Text())
+		bs := []byte(ln)
+		r := rot13(bs)
+		fmt.Fprintf(conn, "%s - %s\n", ln, r)
+	}
+	defer conn.Close()
+}
+
+func rot13(bs []byte) []byte {
+	var r13 = make([]byte, len(bs))
+	for i, v := range bs {
+		// Ascii 97-122
+		if v <= 109 {
+			r13[i] += 13
+		} else {
+			r13[i] -= 13
+		}
+	}
+	return r13
+}
